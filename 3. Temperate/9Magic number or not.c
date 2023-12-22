188abfebdbfbae5a802b807cc40ccfbf0c33df48
mohit30102 <152309964+mohit30102@users.noreply.github.com>
2023-12-22 21:03:43 +0530

    Update 9Magic number or not.c

--- 3. Temperate/9Magic number or not.c
+++ 3. Temperate/9Magic number or not.c
@@ -5,20 +5,18 @@ int main(){
     printf("Enter a Number \n");
     scanf("%d",&n);  
     i = n;
-    //value=1729
-    //Calculating Sum of digits
     while(i > 0){
         sum += i % 10;  
         i = i / 10;      
-    } //sum=19
+    } 
     printf(" \n sum is %d",sum);
-    i = sum; //sum=19=i
+    i = sum;
      while( i > 0){
         rev = rev*10 + i % 10; 
         i = i / 10;
-    } //i=19,rev=91
+    }
     printf(" \n reverse no is %d",rev);
-    if(rev*sum == n) //rev=91*sum=17
+    if(rev*sum == n) 
         printf("\n it is Magic Number \n");
     else
         printf("\n it is Not  Magic Number \n");
